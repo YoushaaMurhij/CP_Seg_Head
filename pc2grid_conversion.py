f77a726ddc99f05d0b6a70cba548a9dcaacb9f8d
youshaamurhij <yosha.morheg@gmail.com>
2021-01-08 02:04:14 +0300

    fixed convertion bug! added map plot

--- pc2grid_conversion.py
+++ pc2grid_conversion.py
@@ -4,26 +4,26 @@ import numpy as np
 from os import listdir
 from os.path import isfile, join
 
+from matplotlib import pyplot as plt
+
 from label_mapping import *
 
-
 def load_cloud_from_deecamp_file(pc_f, lb_f):
-        logging.info('loading cloud from: {} and labels from : {}'.format(pc_f, lb_f))
+        print('loading cloud from: {} and labels from : {}'.format(pc_f, lb_f))
         num_features = 4
         cloud = np.fromfile(pc_f, dtype=np.float32, count=-1).reshape([-1, num_features])
-        # last dimension should be the timestamp.
         label = np.fromfile(lb_f, dtype=np.uint32)
         label = label.reshape((-1))
         cloud = np.hstack((cloud, np.zeros([cloud.shape[0], 1])))
         return cloud, label
 
 def main():
-
     grid_size = 256
-    pos_offset = 51.2 
-    pc_width = 51.2 * 2
+    pos_offset = 80 
+    pc_width = 80 * 2
     num_classes = 33 
-    
+    save_png = True
+
     seg_grid = np.zeros([grid_size, grid_size, num_classes])
     seg_grid.astype(int)
 
@@ -39,16 +39,20 @@ def main():
     for i, (bin, lbl) in enumerate(zip(bin_files, lbl_files)):
         cloud, label = load_cloud_from_deecamp_file(bin_path+"/"+bin, lbl_path+"/"+lbl)
         # print(f'cloud shape os :{np.shape(cloud)}')
-        # print(f'label shape is :{np.shape(label)}')
 
         for j, (pt, lb) in enumerate(zip(cloud, label)):
-            # print(f'indices are {int((pt[0] + pos_offset) * pc_width / grid_size)}, {int((pt[1] + pos_offset) * pc_width / grid_size)}, {class2id[lb]}')
-            if lb>33:
+            if lb > 256:
                 continue
-            seg_grid[int((pt[0] + pos_offset) * pc_width / grid_size), int((pt[1] + pos_offset) * pc_width / grid_size), class2id[lb]]+=1   #TODO bug in grid always zeros
-        
+            # print(f'x = {pt[0]} and y = {pt[1]}')
+            seg_grid[int((pt[0] + pos_offset) * grid_size / pc_width - 1), int((pt[1] + pos_offset) * grid_size / pc_width - 1), class2id[lb]] += 1  
         fin_grid = np.argmax(seg_grid, axis=2)
-        np.savetxt(lbl+'_grd', fin_grid, delimiter=',')
+        np.savetxt(lbl+'_grd', fin_grid,  fmt='%d' , delimiter=',')
+
+        if save_png:
+            plt.figure()
+            plt.imshow(fin_grid, interpolation='nearest')
+            plt.savefig(lbl+'_grd.png')
+            plt.clf()
         
 
 if __name__=="__main__":
