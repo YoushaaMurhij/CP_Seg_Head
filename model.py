16b420d3ef416c639c08ec9dcc76b827b4e44ac1
youshaamurhij <yosha.morheg@gmail.com>
2021-01-11 14:37:17 +0300

    added onnx support

--- model.py
+++ model.py
@@ -30,16 +30,17 @@ class Seg_Head(nn.Module):
             nn.Conv2d(self.mid_layer//2, self.mid_layer//4, kernel_size=self.kernel_size, padding=1, bias=True),
             nn.BatchNorm2d(self.mid_layer//4),
             nn.ReLU(inplace=True),
-            nn.Conv2d(self.mid_layer//4, self.output_size, kernel_size=self.kernel_size, stride=1, padding=1, bias=True)
-            nn.BatchNorm2d(self.mid_layer//2),
+            nn.Conv2d(self.mid_layer//4, self.output_size, kernel_size=self.kernel_size, stride=1, padding=1, bias=True),
+            nn.BatchNorm2d(self.output_size),
             nn.ReLU(inplace=True)
         )
 
     def forward(self, x):
         x = self.conv_head1(x)
+        print(x.shape)
         # torch.Size([1, 64, 128, 128])
-         
-        x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=True)     
+        x = self.up(x)
+        #x = F.interpolate(x, scale_factor=2, mode='bilinear', align_corners=True)     
         # torch.Size([1, 64, 256, 256])
         x = self.conv_head2(x)
         # torch.Size([1, 19, 256, 256])
@@ -50,6 +51,11 @@ def main():
 
     torch_model = Seg_Head()
     x = torch.randn(1, 384, 128, 128, requires_grad=True)
-    #torch_out = torch_model(x)
-    torch.onnx.export(torch_model, x, "Seg_Head.onnx", export_params=True, opset_version=10,          
-                    do_constant_folding=True, input_names = ['input'], output_names = ['output'])
+    torch_out = torch_model(x)
+    print(torch_out.shape)
+    torch.onnx.export(torch_model, x, "Seg_Head.onnx", export_params=True, opset_version=11,          
+                   do_constant_folding=True, input_names = ['input'], output_names = ['output'])
+
+if __name__ == "__main__":
+    main()
+    
