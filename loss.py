51ebbdb8f49cdb888039b623ee91e524d943c8e0
Youshaa Murij <yosha.morheg@gmail.com>
2021-01-25 01:47:10 +0300

    removed losses!

--- loss.py
+++ loss.py
@@ -3,24 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-class FocalLoss(nn.CrossEntropyLoss):
-    ''' Focal loss for classification tasks on imbalanced datasets '''
-
-    def __init__(self, gamma, alpha=None, ignore_index=-100, reduction='none'):
-        super().__init__(weight=alpha, ignore_index=ignore_index, reduction='none')
-        self.reduction = reduction
-        self.gamma = gamma
-
-    def forward(self, input_, target):
-        cross_entropy = super().forward(input_, target)
-        # Temporarily mask out ignore index to '0' for valid gather-indices input.
-        # This won't contribute final loss as the cross_entropy contribution
-        # for these would be zero.
-        target = target * (target != self.ignore_index).long()
-        input_prob = torch.gather(F.softmax(input_, 1), 1, target.unsqueeze(1))
-        loss = torch.pow(1 - input_prob, self.gamma) * cross_entropy
-        return torch.mean(loss) if self.reduction == 'mean' else torch.sum(loss) if self.reduction == 'sum' else loss
-
 class FocalLoss_(nn.Module):
     def __init__(self, gamma=0, alpha=None, size_average=True):
         super(FocalLoss_, self).__init__()
@@ -51,47 +33,3 @@ class FocalLoss_(nn.Module):
         loss = -1 * (1-pt)**self.gamma * logpt
         if self.size_average: return loss.mean()
         else: return loss.sum()
-
-
-class FocalLoss_2(nn.Module):
-    def __init__(self, gamma=2, alpha=0.8, weight=None, size_average=True):
-        super(FocalLoss_2, self).__init__()
-        self.gamma = gamma
-        self.alpha = alpha
-
-    def forward(self, inputs, targets, smooth=1):
-        
-        #comment out if your model contains a sigmoid or equivalent activation layer
-        inputs = F.sigmoid(inputs)       
-        
-        #flatten label and prediction tensors
-        inputs = inputs.view(-1)
-        targets = targets.view(-1)
-        
-        #first compute binary cross-entropy 
-        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
-        BCE_EXP = torch.exp(-BCE)
-        focal_loss = self.alpha * (1-BCE_EXP)**self.gamma * BCE
-                       
-        return focal_loss
-
-
-class DiceBCELoss(nn.Module):
-    def __init__(self, weight=None, size_average=True):
-        super(DiceBCELoss, self).__init__()
-
-    def forward(self, inputs, targets, smooth=1):
-        
-        #comment out if your model contains a sigmoid or equivalent activation layer
-        inputs = F.sigmoid(inputs)       
-        
-        #flatten label and prediction tensors
-        inputs = inputs.view(-1)
-        targets = targets.view(-1)
-        
-        intersection = (inputs * targets).sum()                            
-        dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
-        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
-        Dice_BCE = BCE + dice_loss
-        
-        return Dice_BCE
