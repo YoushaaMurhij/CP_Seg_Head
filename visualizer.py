186d3e73eca91268ab03090cb3ab0bbc695af480
Youshaa Murij <yosha.morheg@gmail.com>
2021-01-31 22:55:08 +0300

    merging 2d

--- visualizer.py
+++ visualizer.py
@@ -14,7 +14,7 @@ def visual2d(grid, index, save_dir, epoch=None):
     if epoch != 'None':
         plt.savefig(save_dir + '/Epoch_' + epoch + '_{:0>7}'.format(index) + '.png')
     else:
-        plt.savefig(save_dir + '/_{:0>7}'.format(index) + '.png')
+        plt.savefig(save_dir + '/{:0>7}'.format(index) + '.png')
     plt.clf()
     plt.close()
 
@@ -22,7 +22,7 @@ def visual(grid, label, save_dir):
     '''Visualizing a sample for the every batch as a picture'''
     plt.figure() #TODO add colors
     plt.imshow(grid, interpolation='bilinear')
-    plt.savefig(save_dir + label + '.png')
+    plt.savefig(save_dir + label[:7] + '.png')
     plt.clf()
     plt.close()
 
@@ -33,7 +33,7 @@ def main():
     # else:
     #     lbl_path=sys.argv[1]
     targets_path = '/home/josh94mur/data/targets'
-    save_dir = '/home/josh94mur/data/figures/'
+    save_dir = '/home/josh94mur/data/'
     targets_files = [f for f in listdir(targets_path) if isfile(join(targets_path, f))]
     targets_files.sort()
     for target in targets_files:
